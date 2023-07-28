211ab857232b3618f5268d79c6e11d10028837a0
fatma-abd-elfatah <fatma.abdelfatah17@gmail.com>
2023-07-28 19:12:53 +0300

    player detail

---
+++ lib/Data/Cubits/cubit/player_detail_state.dart
@@ -0,0 +1,13 @@
+// part of 'player_detail_cubit.dart';
+
+// @immutable
+// abstract class PlayerDetailState {}
+
+// class PlayerDetailLoading extends PlayerDetailState {}
+
+// class PlayerDetailSucsess extends PlayerDetailState {
+//   final SportsApp ourResponse;
+//   PlayerDetailSucsess({required this.ourResponse});
+// }
+
+// class PlayerDetailError extends PlayerDetailState {}
